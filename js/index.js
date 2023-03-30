df51a47b30f84aab5f2bdad7dd7faca0bb13285f
Kimchaerimmm <122502135+simchaewon@users.noreply.github.com>
2023-03-30 12:47:29 +0900

    editted

--- js/index.js
+++ js/index.js
@@ -3,12 +3,19 @@ const bullsEye = document.querySelector('#red-ball');
 const text = document.querySelector('#text p');
 const body = document.querySelector('body');
 
-let focus = document.getElementById('focus');
-let focusHalfWidth= focus.offsetWidth/2;
+const focusObj = document.getElementById('focus');
+let focusPos= {
+    x : focusObj.offsetWidth/2,
+    y : focusObj.offsetHeight/2
+}
 
 document.onmousemove = function(e){
-    focus.style.left = e.pageX - focusHalfWidth + "px";
-    focus.style.top = e.pageY - focusHalfWidth + "px";
+    // focusObj.style.left = e.pageX - focusObjHalfWidth + "px";
+    // focusObj.style.top = e.pageY - focusObjHalfWidth + "px";
+
+    focusObj.style.clipPath = `circle(140px at ${e.pageX - focusObj.getBoundingClientRect().left}px ${e.pageY - focusObj.getBoundingClientRect().top}px)`
+    console.log(e.pageX,focusPos.x)
+    console.log()
 }
 text.innerHTML = text.innerText.split("").map(
     (char, i) => `<span style="transform:rotate(${i * 14}deg)">${char}</span>`).join("")
